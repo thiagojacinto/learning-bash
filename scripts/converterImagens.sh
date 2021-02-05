55200aeaf34666ff2e238206fa9fbefe24fcecda
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-02-04 22:30:33 -0300

    feat: function inside a script & error handling

--- scripts/converterImagens.sh
+++ scripts/converterImagens.sh
@@ -1,15 +1,30 @@
 # !/bin/bash
 cd /workspace/imagens-livros
 
+converte_imagem() {
 if [ ! -d png  ]
 then
-	mkdir png
-	echo "Diretório PNG criado!"
+        mkdir png
+        if [ $? -eq 0 ]
+        then
+                echo "Diretorio PNG criado!"
+        fi
 fi
 
 for imagem in *.jpg
 do
-	nome_da_imagem=$(ls $imagem | awk -F. '{ print $1 }')
-	convert $nome_da_imagem.jpg png/$nome_da_imagem.png
-	echo "Imagem convertida:"+$nome_da_imagem
+        local nome_da_imagem=$(ls $imagem | awk -F. '{ print $1 }')
+        convert $nome_da_imagem.jpg png/$nome_da_imagem.png
+        echo "Imagem convertida: " $nome_da_imagem
 done
+}
+
+converte_imagem 2>errors.txt
+
+if [ $? -eq 0 ] 
+then 
+        echo "Conversion was successful. Checkout png output directory"
+else 
+        echo "Ops, something wrong happen. We might chekout more information at errors.txt"
+fi
+
