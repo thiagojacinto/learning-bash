87f6b72ff372b12b145b831e68e0dd73a31566c8
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-02-09 21:36:03 -0300

    feat: sorting process script now listing by name & creating a log file

--- scripts/listarProcessos.sh
+++ scripts/listarProcessos.sh
@@ -4,5 +4,7 @@ processList=$(ps -e -o pid --sort -size | head -n 11 | grep [0-9])
 
 for proc in $processList
 do
-  echo $(ps -p $proc -o comm=)
+        pname=$(ps -p $proc -o comm=)
+        echo $pname
+        echo $(date +%F,%H:%M:%S,) >> $pname.log
 done
