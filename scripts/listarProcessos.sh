4e7ff4dfa76c89324ed49cff9ad0d4ef94d15537
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-02-09 22:06:29 -0300

    feat: sorting proc. script now calculating memory and saving log

--- scripts/listarProcessos.sh
+++ scripts/listarProcessos.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+if [ ! -d logs ]
+then
+        mkdir logs
+fi
+
+listar(){
 processList=$(ps -e -o pid --sort -size | head -n 11 | grep [0-9])
 
 for proc in $processList
 do
         pname=$(ps -p $proc -o comm=)
-        echo $pname
-        echo $(date +%F,%H:%M:%S,) >> $pname.log
+        psize=$(ps -p $proc -o size | grep [0-9])
+        echo -n $(date +%F,%H:%M:%S,) >> logs/$pname.log
+        echo "$(bc <<< "scale=2;$psize/1024")MB" >> logs/$pname.log
+
 done
+}
+
+listar 2>>Errors.log
+if [ $? -eq 0 ]
+then
+        echo "Success! Verify /logs to see the results"
+else
+        echo "Ops, an Error has been found. Verify Errors.log for more information."
+fi
